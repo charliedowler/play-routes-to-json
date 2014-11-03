6304b6a44123247c26b96c0397760adee7acd8ff
charliedowler <charliedowler15@gmail.com>
2014-11-03 23:50:00 +0000

    Setup tests
    tweaks

--- test.js
+++ test.js
@@ -1,21 +1,36 @@
 var routesToJSON = require('./');
+var test = require('tap').test;
 var fs = require('fs');
 
-function testFailed(err) {
-  throw new Error(err);
-}
-
-// TODO: Setup proper tests
 fs.readFile('testRoutes', function(err, data) {
   var result = JSON.parse(routesToJSON(data.toString())).routes;
   var zero = result[0];
   var one = result[1];
   var two = result[2];
   var three = result[3];
-  if (zero.type != 'GET' || zero.path != '/' || zero.controller != 'controllers.Application.index()') testFailed('Zero failed');
-  if (one.type != 'GET' || one.path != '/assets/*file' || one.controller != 'controllers.Assets.at(path="/public",file)') testFailed('One failed');
-  if (two.type != 'GET' || two.path != '/path/to/something' || two.controller != '@controllers.package.class.method()') testFailed('Two failed');
-  if (three.type != 'POST' || three.path != '/path/to/something' || three.controller != '@controllers.package.class.method(issue_id:Integer,issue_key,project_id:Integer,project_key)') testFailed('Three failed');
-  console.log('All tests passed');
-  process.exit(0);
+
+  test('route zero parsed', function(t) {
+    t.equal(zero.type, 'GET');
+    t.equal(zero.path, '/');
+    t.equal(zero.controller, 'controllers.Application.index()');
+    t.end();
+  });
+  test('route one parsed', function(t) {
+    t.equal(one.type, 'GET');
+    t.equal(one.path, '/assets/*file');
+    t.equal(one.controller, 'controllers.Assets.at(path="/public",file)');
+    t.end();
+  });
+  test('route two parsed', function(t) {
+    t.equal(two.type, 'GET');
+    t.equal(two.path, '/path/to/something');
+    t.equal(two.controller, '@controllers.package.class.method()');
+    t.end();
+  });
+  test('route three parsed', function(t) {
+    t.equal(three.type, 'POST');
+    t.equal(three.path, '/path/to/something');
+    t.equal(three.controller, '@controllers.package.class.method(issue_id:Integer,issue_key,project_id:Integer,project_key)');
+    t.end();
+  });
 });
